c34b7236789ef6712f853e88d64f650e166ad416
Błażej Blaut <blazbla274@student.polsl.pl>
2019-03-31 17:02:19 +0200

    add header(welcome message) under the box

--- repair-management-system/src/components/LoginForm/LoginForm.js
+++ repair-management-system/src/components/LoginForm/LoginForm.js
@@ -4,6 +4,9 @@ import './LoginForm.css';
 const LoginForm = () => {
     return (
       <div className="box">
+          <div className="header">
+               <h1>Welcome in CarRepair system</h1>
+          </div>
           <input type="text" placeholder="Login"/>
           <input type="password" placeholder="Password" />
           <input type="submit" value="Submit" />
