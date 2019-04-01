068bb5e4dec981e64db695d3fbc29b6c5fce2d04
Błażej Blaut <blazbla274@student.polsl.pl>
2019-04-01 19:47:47 +0200

    add app status, and set loged variable default flase

--- repair-management-system/src/App.js
+++ repair-management-system/src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import LoginForm from './components/LoginForm/LoginForm';
 
 class App extends Component {
+  state = {
+    loged: false
+  }
+
   render() {
     return (
       <div className="App">
-          <LoginForm />
+        <LoginForm loged={this.state.loged}/>
       </div>
     );
   }
--- repair-management-system/src/components/LoginForm/LoginForm.js
+++ repair-management-system/src/components/LoginForm/LoginForm.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './LoginForm.css';
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+  if(!props.loged)
     return (
       <div className="box">
           <div className="header">
-               <h1>Welcome in CarRepair system</h1>
+            <h1>Welcome in CarRepair system</h1>
           </div>
           <input type="text" placeholder="Login"/>
           <input type="password" placeholder="Password" />
-          <input type="submit" value="Submit" />
+          <input type="submit" value="Submit"/>
       </div>
     );
+  else
+    return null
 }
 
 export default LoginForm;
