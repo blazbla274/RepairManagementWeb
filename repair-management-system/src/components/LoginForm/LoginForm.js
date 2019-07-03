138b5323045cb3fead146e784eb10f541a0bac36
Błażej Blaut <blauto@interia.pl>
2019-07-03 22:05:50 +0200

    Remove login and password inputs and submit.

--- repair-management-system/src/components/LoginForm/LoginForm.js
+++ repair-management-system/src/components/LoginForm/LoginForm.js
@@ -15,26 +15,28 @@ const LoginForm = (props) => {
 
   if(!props.loged)
     return (
-      <div className="box">
-          <div className="header">
+      <div className="box box--only-facebook">
+          <div className="header header--only-facebook">
             <h1 style={props.style.fontColor}>Welcome in CarRepair system</h1>
           </div>
-          <input 
-            type="text" 
-            placeholder="Login" 
-            style={{...props.style.inputBackgroundColor, ...props.style.borderColor}}
-            onChange={handleLoginChange}/>
-          <input 
-            type="password" 
-            placeholder="Password" 
-            style={{...props.style.inputBackgroundColor, ...props.style.borderColor}}
-            onChange={handlePasswordChange}/>
-          <input 
-            type="submit" 
-            value="Submit" 
-            onClick={() => props.loginAction(login, password)}
-            style={{...props.style.backgroundColor, ...props.style.borderColor}}/>
-          <a href="http://localhost:8080/oauth2/authorize/facebook?redirect_uri=http://localhost:3000">Log in using Facebook.</a>
+          {
+            //<input 
+            //type="text" 
+            //placeholder="Login" 
+            //style={{...props.style.inputBackgroundColor, ...props.style.borderColor}}
+            //onChange={handleLoginChange}/>
+          //<input 
+            //type="password" 
+            //placeholder="Password" 
+            //style={{...props.style.inputBackgroundColor, ...props.style.borderColor}}
+            //onChange={handlePasswordChange}/>
+          //<input 
+            //type="submit" 
+            //value="Submit" 
+            //onClick={() => props.loginAction(login, password)}
+            //style={{...props.style.backgroundColor, ...props.style.borderColor}}/>
+          }
+          <a className="--only-facebook" href="http://localhost:8080/oauth2/authorize/facebook?redirect_uri=http://localhost:3000">Log in using Facebook.</a>
       </div>
     );
   else
